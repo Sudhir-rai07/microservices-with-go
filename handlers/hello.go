6652471368caf843614d03e068f9932c3234ac51
Sudhir-rai07 <sudhirrai128@gmail.com>
2024-12-23 14:21:44 +0530

    make server with ServerMux and Server graceful shutdown

---
+++ handlers/hello.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+func (h *Hello) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	h.l.Printf("Hello sir")
+	rw.Write([]byte("Hello sir"))
+}
--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/Sudhir-rai07/microservices-with-go/handlers"
 )
 
 func main() {
+	fmt.Print("Happy Coding")
+	l := log.New(os.Stdout, "Product-api", log.LstdFlags)
 
-	http.HandleFunc("/", handleHomeRoute)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
-	err := http.ListenAndServe(":8808", nil)
-	if err != nil {
-		log.Fatal("Error : ", err)
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":8808",
+		Handler:      sm,
+		IdleTimeout:  30 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
-}
 
-func handleHomeRoute(rw http.ResponseWriter, r *http.Request) {
-	res := []byte("Happy Coding")
-	rw.Write(res)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			log.Fatal("Error : ", err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Printf("Recieved terminate, Graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	s.Shutdown(tc)
 }
